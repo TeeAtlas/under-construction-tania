79644b92b247be6e33d35ccdb808b4330c3ef557
Tania Boterman <taniaboterman@gmail.com>
2025-03-31 20:33:49 +0200

    debugging tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+      "./index.html", // Include your main HTML file
+      "./src/**/*.{js,jsx,ts,tsx}", // Include all files in the src folder
+    ],
+    theme: {
+      extend: {}, // You can customize your theme here
+    },
+    plugins: [], // Add plugins here if needed
+  };
