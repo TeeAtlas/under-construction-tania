93f099fa876c50cb5b271342fda44981dc12989b
Tania Boterman <taniaboterman@gmail.com>
2025-03-31 11:35:44 +0200

    basic project started

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css"
+
+export default function App() {
+    return (<div>
+        <h1>
+            Hello, I'm Tania and I'm Under Construction
+        </h1>
+        <p>Please follow the links for more information :)
+        </p>
+    </div>)
+}
